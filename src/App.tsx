8d52019903b342d8d51f12bd10062e2132a93280
Mukezh <mukesh.offcl@gmail.com>
2025-02-20 17:54:46 +0530

    portfolio

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import LoadingScreen from './components/Landing'
+
+function App() {
+  
+
+  return (
+    <div>
+      <LoadingScreen />
+    </div>
+  )
+}
+
+export default App
